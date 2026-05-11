5205d7fbc51406779818717408a0b2dc5a554895
Stefano Videsott <ste.videsott@gmail.com>
2026-05-11 11:43:52 +0200

    feat: schema and data population update

--- main_webapp/src/main/resources/data.sql
+++ main_webapp/src/main/resources/data.sql
@@ -3,47 +3,27 @@ DELETE FROM USERS;
 DELETE FROM USERDATA;
 
 -- Admin: password = adm_id_07
-INSERT INTO USERS (username, password, enabled) VALUES
-    ('admin#07', '$2a$10$Yw2oRKktnyJPPSdBCJ3slOg/AzoXjNHmkIATa3n21YxVRfXBpuuku', 1);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('admin#07', 'ROLE_ADMIN');
-
 -- Basic: password = bsc_id_07
-INSERT INTO USERS (username, password, enabled) VALUES
-    ('basic#07', '$2a$10$QPUP9UQu2af39luCl32Pp.ki/LFwDsBhG2BTbn4sMpBL0e0iO7tWO', 1);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('basic#07', 'ROLE_USER_BASIC');
-
 -- Pro: password = pro_id_07
-INSERT INTO USERS (username, password, enabled) VALUES
-    ('pro#07', '$2a$10$cDGeQY57lzax0.rRufKT7.E0MHOOlMwuLlrk4qwvS73JtoV2Pc0em', 1);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('pro#07', 'ROLE_USER_PRO');
-
 -- Utenti PROVA: password = prv_id_07
 INSERT INTO USERS (username, password, enabled) VALUES
-    ('prova#1#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 1);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('prova#1#07', 'ROLE_USER_PROVA');
-
-INSERT INTO USERS (username, password, enabled) VALUES
-    ('prova#2#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 1);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('prova#2#07', 'ROLE_USER_PROVA');
-
-INSERT INTO USERS (username, password, enabled) VALUES
-    ('prova#3#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 0);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('prova#3#07', 'ROLE_USER_PROVA');
-
-INSERT INTO USERS (username, password, enabled) VALUES
-    ('prova#4#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 0);
-INSERT INTO AUTHORITIES (username, authority) VALUES
-    ('prova#4#07', 'ROLE_USER_PROVA');
-
-INSERT INTO USERS (username, password, enabled) VALUES
+    ('admin#07', '$2a$10$Yw2oRKktnyJPPSdBCJ3slOg/AzoXjNHmkIATa3n21YxVRfXBpuuku', 1),
+    ('basic#07', '$2a$10$QPUP9UQu2af39luCl32Pp.ki/LFwDsBhG2BTbn4sMpBL0e0iO7tWO', 1),
+    ('pro#07', '$2a$10$cDGeQY57lzax0.rRufKT7.E0MHOOlMwuLlrk4qwvS73JtoV2Pc0em', 1),
+    ('prova#1#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 1),
+    ('prova#2#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 1),
+    ('prova#3#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 0),
+    ('prova#4#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 0),
     ('prova#5#07', '$2a$10$VkC.v783OrhGMZP3HbKFm.FdoRG38F4Fm9bIynJSlqAbtvq0ie2uK', 1);
+
 INSERT INTO AUTHORITIES (username, authority) VALUES
+    ('admin#07', 'ROLE_ADMIN'),
+    ('basic#07', 'ROLE_USER_BASIC'),
+    ('pro#07', 'ROLE_USER_PRO'),
+    ('prova#1#07', 'ROLE_USER_PROVA'),
+    ('prova#2#07', 'ROLE_USER_PROVA'),
+    ('prova#3#07', 'ROLE_USER_PROVA'),
+    ('prova#4#07', 'ROLE_USER_PROVA'),
     ('prova#5#07', 'ROLE_USER_PROVA');
 
 INSERT INTO USERDATA (username, firstName, lastName, email, birthDate, signupDate) VALUES
